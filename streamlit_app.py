4ce9c59959a9ab8392cda10f26223bb6faed231e
Dominic Christopher <christopherdominic268@gmail.com>
2026-04-03 10:07:50 -0500

    Corrected safeguard again

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,44 +40,46 @@ if eta_call.status_code == 200:
 
     if not eta['eta_minutes']:
         st.warning(f"Sorry, we couldn't calculate the ETA for route {selected_route_id} at {selected_stop_name} at this time.")
-        
-    next_bus = eta['eta_minutes'][0]
-
-    st.metric("Next Bus Arrival", value=f"{round(next_bus,0)} minutes" if next_bus >= 1 else "Now") #Display the ETA for the next bus arrival using a metric component. The label is set to "Next Bus Arrival" and the value is formatted to show the ETA in minutes if it is greater than or equal to 1 minute, or "Now" if the ETA is less than 1 minute.
     
-    for i in range(1,(len(eta['eta_minutes']))):
-        eta_info = round(eta['eta_minutes'][i],0)
-        vehicle_id = eta['vehicle_id'][i]
-        if eta_info >= 1:
-            st.write(f"{eta_info} minutes.")
-        elif eta_info < 1:
-            st.write(f"Bus {vehicle_id} now.")
+    else:
 
-    # if 'eta_minutes' not in eta.keys():
-    #     st.write(f"Sorry, we couldn't calculate the ETA for route {selected_route_id} at {selected_stop_name} at this time.")
-    # elif eta['eta_minutes'] >= 1:
-    #     st.write(f"Bus {eta['vehicle_id']}  on route {selected_route_id} is expected to arrive at {selected_stop_name} in {eta['eta_minutes']} minutes.")
-    #     # st.write(f"{eta}")
-    # elif eta['eta_minutes'] < 1:
-    #     st.write(f"Bus {eta['vehicle_id']} on route {selected_route_id}  is expected to arrive at {selected_stop_name} now.")
+        next_bus = eta['eta_minutes'][0]
 
-    #Collect locations for stops and next buses
-    stop_loc = requests.get(f"http://localhost:5000/stop/location?stop_id={stop['stop_id']}&route_id={selected_route_id}").json() #Make a GET request to the backend API to fetch the location of the vehicle corresponding to the next bus arrival. The request includes the vehicle ID as a query parameter, which is extracted from the ETA data received from the previous API call. The response from the API is expected to contain the location information for the specified vehicle, which can be used to display the current location of the next bus on a map or provide additional information to the user.
-    vehicle_locs = []
-    for id in eta['vehicle_id']:
-        loc = requests.get(f"http://localhost:5000/location/vehicle?vehicle_id={id}").json()
-        vehicle_locs.append(loc)
+        st.metric("Next Bus Arrival", value=f"{round(next_bus,0)} minutes" if next_bus >= 1 else "Now") #Display the ETA for the next bus arrival using a metric component. The label is set to "Next Bus Arrival" and the value is formatted to show the ETA in minutes if it is greater than or equal to 1 minute, or "Now" if the ETA is less than 1 minute.
+        
+        for i in range(1,(len(eta['eta_minutes']))):
+            eta_info = round(eta['eta_minutes'][i],0)
+            vehicle_id = eta['vehicle_id'][i]
+            if eta_info >= 1:
+                st.write(f"{eta_info} minutes.")
+            elif eta_info < 1:
+                st.write(f"Bus {vehicle_id} now.")
+
+        # if 'eta_minutes' not in eta.keys():
+        #     st.write(f"Sorry, we couldn't calculate the ETA for route {selected_route_id} at {selected_stop_name} at this time.")
+        # elif eta['eta_minutes'] >= 1:
+        #     st.write(f"Bus {eta['vehicle_id']}  on route {selected_route_id} is expected to arrive at {selected_stop_name} in {eta['eta_minutes']} minutes.")
+        #     # st.write(f"{eta}")
+        # elif eta['eta_minutes'] < 1:
+        #     st.write(f"Bus {eta['vehicle_id']} on route {selected_route_id}  is expected to arrive at {selected_stop_name} now.")
+
+        #Collect locations for stops and next buses
+        stop_loc = requests.get(f"http://localhost:5000/stop/location?stop_id={stop['stop_id']}&route_id={selected_route_id}").json() #Make a GET request to the backend API to fetch the location of the vehicle corresponding to the next bus arrival. The request includes the vehicle ID as a query parameter, which is extracted from the ETA data received from the previous API call. The response from the API is expected to contain the location information for the specified vehicle, which can be used to display the current location of the next bus on a map or provide additional information to the user.
+        vehicle_locs = []
+        for id in eta['vehicle_id']:
+            loc = requests.get(f"http://localhost:5000/location/vehicle?vehicle_id={id}").json()
+            vehicle_locs.append(loc)
 
 
-    data = {
-        "name":[stop['stop_id']] + eta['vehicle_id'],
-        "lat":[stop_loc['lat']] + [loc['lat'] for loc in vehicle_locs],
-        "lon":[stop_loc['lon']] + [loc['lon'] for loc in vehicle_locs]
-    }
+        data = {
+            "name":[stop['stop_id']] + eta['vehicle_id'],
+            "lat":[stop_loc['lat']] + [loc['lat'] for loc in vehicle_locs],
+            "lon":[stop_loc['lon']] + [loc['lon'] for loc in vehicle_locs]
+        }
 
-    map_data = pd.DataFrame(data)
+        map_data = pd.DataFrame(data)
 
-    st.map(map_data, zoom=12) #Display a map centered around the location of the next bus arrival, using the latitude and longitude information fetched from the backend API. The map is created using the st.map() function, which takes a DataFrame containing the latitude and longitude data for the stop and the next bus arrivals. The zoom level is set to 12 to provide a clear view of the area around the stop and the next bus locations.
-    # st.write(data)
+        st.map(map_data, zoom=12) #Display a map centered around the location of the next bus arrival, using the latitude and longitude information fetched from the backend API. The map is created using the st.map() function, which takes a DataFrame containing the latitude and longitude data for the stop and the next bus arrivals. The zoom level is set to 12 to provide a clear view of the area around the stop and the next bus locations.
+        # st.write(data)
 else:
     st.warning("No active buses on this route.")
