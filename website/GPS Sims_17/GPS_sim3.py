d5af994cdf4f3e0b94cb168b2dae358f8bc8cde8
Dominic Christopher <christopherdominic268@gmail.com>
2026-04-05 13:35:02 -0500

    Fixed gps sim with open line

--- website/GPS Sims_17/GPS_sim3.py
+++ website/GPS Sims_17/GPS_sim3.py
@@ -8,7 +8,7 @@ load_dotenv() #Load environment variables from .env file
 URL = 'http://127.0.0.1:5000/location'
 
 your_api_key = os.getenv('VEHICLE_3_KEY') #API key for authentication, generated when vehicle is added to database. Can be found in the vehicles table in the database
-with open('coords2.txt', 'r') as f:
+with open('website/GPS Sims_17/coords2.txt', 'r') as f:
     list_of_coords = f.read()
 
 route17 = ast.literal_eval(list_of_coords)
