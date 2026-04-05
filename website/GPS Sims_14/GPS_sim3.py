0bbb85e0d2fc9def3c55c12ac329f819b342b4d5
Dominic Christopher <christopherdominic268@gmail.com>
2026-04-05 18:25:07 -0500

    Foxong route 13 and 14 file directories

--- website/GPS Sims_14/GPS_sim3.py
+++ website/GPS Sims_14/GPS_sim3.py
@@ -8,7 +8,7 @@ load_dotenv() #Load environment variables from .env file
 URL = "http://127.0.0.1:5000/location" #Backend url
 
 your_api_key = os.getenv('VEHICLE_7_KEY') #API key for authentication, generated when vehicle is added to database. Can be found in the vehicles table in the database
-with open('coords14.txt', 'r') as f:
+with open('website/GPS Sims_14/coords14.txt', 'r') as f:
     list_of_coords = f.read()
 
 route = ast.literal_eval(list_of_coords)
