04f575965b748e7d496594a3ba0755c11783dace
Dominic Christopher <christopherdominic268@gmail.com>
2026-04-09 10:27:26 -0500

    Ensuring database tables are only created when local env used

--- website/__init__.py
+++ website/__init__.py
@@ -14,24 +14,34 @@ def create_app():
     app = Flask(__name__)
     os.makedirs(app.instance_path, exist_ok=True)
 
-    load_dotenv()
+    #Only takes my local env variable if railway env not available
+    if os.getenv("RAILWWAY_ENVIRONMENT") is None:
+        load_dotenv()
 
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    if not DATABASE_URL:
+        logging.error("DATABASE_URL environment variable not set.")
+        raise ValueError("DATABASE_URL environment variable not set.")
+    
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+        
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
     # initialize database
     db.init_app(app)
 
     #import tables from models.py
     from .models import Vehicle, Stops, Routes, Routes_Stops
-
     #register blueprints
     from .views import views
     app.register_blueprint(views, url_prefix = '/')
     
-    #create database
-    with app.app_context():
-        db.create_all()
+    if os.getenv("RAILWAY_ENVIRONMENT") is None: 
+        logging.info("Running in local environment.")
+        with app.app_context():
+            db.create_all()
 
     return app
 
